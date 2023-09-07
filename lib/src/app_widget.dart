1c3358c0b5e237e20f7a13243fe005773c569a20
Daniel Otavio Todt <danieltodt@gmail.com>
2023-09-07 13:39:39 -0300

    refactor: app widget and board page

---
+++ lib/src/app_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:todo/src/pages/board_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const BoardPage(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
