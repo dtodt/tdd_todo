cec60143d41a2c78c6328a1ebc91a96773854dc3
Daniel Otavio Todt <danieltodt@gmail.com>
2023-09-07 14:19:28 -0300

    test: board page cubit increment

--- lib/src/pages/board_page.dart
+++ lib/src/pages/board_page.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo/src/cubits/board_cubit.dart';
 
 class BoardPage extends StatefulWidget {
   const BoardPage({super.key});
@@ -10,10 +14,22 @@ class BoardPage extends StatefulWidget {
 class _BoardPageState extends State<BoardPage> {
   @override
   Widget build(BuildContext context) {
+    final cubit = context.watch<BoardCubit>();
+    final state = cubit.state;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Tasks'),
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    scheduleMicrotask(() {
+      context.read<BoardCubit>().fetchTasks();
+    });
   }
 }
--- test/src/pages/board_page_test.dart
+++ test/src/pages/board_page_test.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:todo/src/cubits/board_cubit.dart';
 import 'package:todo/src/pages/board_page.dart';
+import 'package:todo/src/repositories/board_repository.dart';
+
+class BoardRepositoryMock extends Mock implements BoardRepository {}
 
 void main() {
+  final repository = BoardRepositoryMock();
+  late BoardCubit cubit;
+
+  setUp(() {
+    cubit = BoardCubit(repository);
+  });
+
+  tearDown(() {
+    reset(repository);
+  });
+
   testWidgets('BoardPage', (tester) async {
-    await tester.pumpWidget(const MaterialApp(home: BoardPage()));
+    await tester.pumpWidget(BlocProvider.value(
+      value: cubit,
+      child: const MaterialApp(home: BoardPage()),
+    ));
   });
 }
