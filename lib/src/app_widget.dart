41910894b2bb0e12254af94546ea32a48ad6eb68
Daniel Otavio Todt <danieltodt@gmail.com>
2023-09-07 13:50:48 -0300

    feat: dependency injection setup

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo/src/cubits/board_cubit.dart';
 import 'package:todo/src/pages/board_page.dart';
+import 'package:todo/src/repositories/board_repository.dart';
+import 'package:todo/src/repositories/isar/isar_board_repository.dart';
+import 'package:todo/src/repositories/isar/isar_datasource.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const BoardPage(),
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        RepositoryProvider(create: (ctx) => IsarBoardDatasource()),
+        RepositoryProvider<BoardRepository>(
+          create: (ctx) => IsarBoardRepository(ctx.read()),
+        ),
+        BlocProvider(create: (ctx) => BoardCubit(ctx.read())),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: const BoardPage(),
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
       ),
     );
   }
