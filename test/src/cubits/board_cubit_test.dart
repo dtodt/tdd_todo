f8a5c355249305db56baac43ec9e6c6bbe42f384
Daniel Otavio Todt <danieltodt@gmail.com>
2023-09-07 09:11:24 -0300

    test: add task

--- test/src/cubits/board_cubit_test.dart
+++ test/src/cubits/board_cubit_test.dart
@@ -7,6 +7,8 @@ import 'package:todo/src/states/board_state.dart';
 
 class BoardRepositoryMock extends Mock implements BoardRepository {}
 
+const task = Task(id: 1, description: 'description');
+
 void main() {
   final repository = BoardRepositoryMock();
   final cubit = BoardCubit(repository);
@@ -16,11 +18,7 @@ void main() {
   group('fetchTasks |', () {
     test('should retrieve all tasks', () async {
       // Arrange
-      when(
-        () => repository.fetch(),
-      ).thenAnswer(
-        (_) async => [const Task(id: 1, description: 'description')],
-      );
+      when(() => repository.fetch()).thenAnswer((_) async => []);
 
       // Assert
       expect(
@@ -54,7 +52,40 @@ void main() {
       await cubit.fetchTasks();
     });
   });
-  // group('addTask |', () { });
+
+  group('addTask |', () {
+    test('should add a task', () async {
+      // Arrange
+      when(() => repository.update([task])).thenAnswer((_) async => []);
+
+      // Assert
+      expect(
+        cubit.stream,
+        emitsInOrder([
+          isA<LoadedBoardState>(),
+        ]),
+      );
+
+      // Act
+      await cubit.addTask(task);
+    });
+
+    test('should emit failure on error', () async {
+      // Arrange
+      when(() => repository.update([task])).thenThrow(Exception());
+
+      // Assert
+      expect(
+        cubit.stream,
+        emitsInOrder([
+          isA<FailureBoardState>(),
+        ]),
+      );
+
+      // Act
+      await cubit.addTask(task);
+    });
+  });
   // group('removeTask |', () { });
   // group('checkTask |', () { });
 }
