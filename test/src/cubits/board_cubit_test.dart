67988a03a351382eab017f9e833c314b2bbf4774
Daniel Otavio Todt <danieltodt@gmail.com>
2023-09-07 09:55:29 -0300

    test: check task

--- test/src/cubits/board_cubit_test.dart
+++ test/src/cubits/board_cubit_test.dart
@@ -8,6 +8,7 @@ import 'package:todo/src/states/board_state.dart';
 class BoardRepositoryMock extends Mock implements BoardRepository {}
 
 const task = Task(id: 1, description: 'description');
+const taskChecked = Task(id: 1, description: 'description', checked: true);
 
 void main() {
   final repository = BoardRepositoryMock();
@@ -135,5 +136,44 @@ void main() {
       await cubit.removeTask(task);
     });
   });
-  // group('checkTask |', () { });
+
+  group('checkTask |', () {
+    test('should check a task', () async {
+      // Arrange
+      when(() => repository.update([taskChecked])).thenAnswer((_) async => []);
+      cubit.addTasks([task]);
+
+      // Assert
+      expect(
+        cubit.stream,
+        emitsInOrder([
+          isA<LoadedBoardState>(),
+        ]),
+      );
+
+      // Act
+      await cubit.checkTask(task);
+
+      // Assert
+      final state = cubit.state as LoadedBoardState;
+      expect(state.tasks, hasLength(1));
+      expect(state.tasks, [taskChecked]);
+    });
+
+    test('should emit failure on error', () async {
+      // Arrange
+      when(() => repository.update([task])).thenThrow(Exception());
+
+      // Assert
+      expect(
+        cubit.stream,
+        emitsInOrder([
+          isA<FailureBoardState>(),
+        ]),
+      );
+
+      // Act
+      await cubit.checkTask(task);
+    });
+  });
 }
